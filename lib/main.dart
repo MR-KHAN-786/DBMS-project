c668d329867889b7e03af01609a3093e5a4e6036
Abdullah Khan <MR-KHAn-786@github.com>
2024-06-23 16:18:06 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:watch_shopping_app/HomeScreen/home_screen.dart';
+import 'package:watch_shopping_app/SplashScreen/splash_screen.dart';
+
+void main() {
+  runApp(const MyWatchapp());
+}
+
+class MyWatchapp extends StatelessWidget {
+  const MyWatchapp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: Splashscreen(),
+      home: Home_screen(),
+      
+      );
+  }
+  
+  }
